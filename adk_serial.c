165796b27da65c53e9a21783bc8bc8b3ad33c715
Pasi Saarinen <phasip@gmail.com>
2015-07-10 22:31:06 +0200

    Only claim accessory interface

--- adk_serial.c
+++ adk_serial.c
@@ -5,11 +5,17 @@
 #include <linux/usb/serial.h>
 #include <linux/uaccess.h>
 
+#define ANDROID_ACCESSORY_VID 0x18d1
+#define ANDROID_ACCESSORY_INTERFACE_CLASS 0xff
+#define ANDROID_ACCESSORY_INTERFACE_SUBCLASS 0xff
+#define ANDROID_ACCESSORY_INTERFACE_PROTO 0x00
+
 static const struct usb_device_id id_table[] = {
-	{ USB_DEVICE(0x18d1, 0x2d01) },
+	{ USB_VENDOR_AND_INTERFACE_INFO(ANDROID_ACCESSORY_VID, ANDROID_ACCESSORY_INTERFACE_CLASS,ANDROID_ACCESSORY_INTERFACE_SUBCLASS,ANDROID_ACCESSORY_INTERFACE_PROTO) },
 	{ },
 };
 MODULE_DEVICE_TABLE(usb, id_table);
+
 
 static struct usb_serial_driver adk_serial_device = {
 	.driver = {
